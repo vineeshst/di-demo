489a6ee8d2d0c07c17a6c57af662f29d24715a04
vineeshst <vineeshstruts@gmail.com>
2019-11-29 13:09:06 +0100

    Property Source Configuration

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,25 +1,33 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+    private final Environment env;
     @Value("${guru.username}")
     String user;
     @Value("${guru.password}")
     String password;
     @Value("${guru.dburl}")
     String url;
+
+    public PropertyConfig(Environment env) {
+        this.env = env;
+    }
+
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setDburl(url);
         return fakeDataSource;
